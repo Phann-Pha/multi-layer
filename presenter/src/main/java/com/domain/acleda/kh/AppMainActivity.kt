d7feee82ffa430e6d5b28f7e867fd9b65ab5f021
Phann-Pha <phannpha.fa@gmail.com>
2025-02-12 21:25:52 +0700

    test hello world

--- presenter/src/main/java/com/domain/acleda/kh/AppMainActivity.kt
+++ presenter/src/main/java/com/domain/acleda/kh/AppMainActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class AppMainActivity : ComponentActivity()
 {
@@ -12,7 +17,9 @@ class AppMainActivity : ComponentActivity()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-        
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text(text = "Hello world!")
+            }
         }
     }
 }
